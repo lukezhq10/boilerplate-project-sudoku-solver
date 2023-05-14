864c96b1066150e06b5946cf201ae649c002644e
Luke Zhang <you@example.com>
2023-05-13 21:22:25 -0600

    added SudokuSolver logic and validate function unit tests

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,22 +1,102 @@
 class SudokuSolver {
 
+  // The validate function should take a given puzzle string and check it to see if it has 81 valid characters for the input
   validate(puzzleString) {
+    if (puzzleString.length !== 81) {
+      console.log('puzzleString invalid length');
+      return false;
+    }
+
+    for (let i = 0; i < puzzleString.length; i++) {
+      if (!/[1-9.]/.test(puzzleString.charAt(i))) {
+        console.log('puzzleString invalid character used');
+        return false;
+      }
+    }
+
+    return true;
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  // check if value is already in the row
+  checkRowPlacement(puzzleString, row, value) {
+    for (let i = 0; i < 9; i++) {
+      if (puzzleString[row * 9 + i] === value) {
+        console.log('invalid row placement');
+        return false;
+      }
+    }
 
+    return true;
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkColPlacement(puzzleString, col, value) {
+    for (let i = 0; i < 9; i++) {
+      if (puzzleString[col + i * 9] === value) {
+        console.log('invalid column placement');
+        return false;
+      }
+    }
 
+    return true;
   }
 
-  checkRegionPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, col, value) {
+    // calculate top-left row and column of the 3x3 box
+    const boxRow = Math.floor(row / 3) * 3;
+    const boxCol = Math.floor(col / 3) * 3;
 
+    // check if value is already in 3x3 box
+    for (let i = boxRow; i < boxRow + 3; i++) {
+      for (let j = boxCol; j < boxCol + 3; j++) {
+        if (puzzleString[i * 9 + j] === value) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 
   solve(puzzleString) {
-    
+    // convert puzzleString to an array for easier manipulation
+    const puzzle = pzuzleString.split('');
+
+    // define a recursive function to solve the puzzle
+    function solveRecursive(index) {
+      // if all cells are solved, return true
+      if (index >= 81) {
+        return true;
+      }
+
+      // if current cell is not empty, move on to next cell
+      if (puzzle[index] !== '.') {
+        return solveRecursive(index + 1);
+      }
+
+      // try each possible value for the current cell
+      for (let value = 1; value <= 9; value++) {
+        // check if current value is valid for this cell
+        if (this.checkRowPlacement(puzzleString, Math.floor(index / 9), value) &&
+            this.checkColPlacement(puzzleString, index % 9, value) &&
+            this.checkRegionPlacement(puzzleString, Math.floor(index / 9), index % 9, value)) {
+          // if current value is valid, set it and move on to next cell
+          puzzle[index] = value;
+          if (solveRecursive(index + 1)) {
+            return true;
+          }
+        }
+      }
+
+      // if no values are valid for this cell, backtrack to previous cell
+      puzzle[index] = '.';
+      return false;
+    }
+
+    // call recursive solver starting at index 0
+    solveRecursive(0);
+
+    // convert puzzle array back to a string and return it
+    return puzzle.join('');
   }
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,24 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
 
 suite('Unit Tests', () => {
+    // Logic handles a valid puzzle string of 81 characters
+    test('valid puzzle string', () => {
+        let input = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+        assert.equal(solver.validate(input), true);
+    });
 
+    // Logic handles a puzzle string with invalid characters (not 1-9 or .)
+    test('invalid puzzle string', () => {
+        let input = '1.a..2.84..63.12.7.b..5.....9..1....8.2.367@.3.7.2..9.47...8..1..-6....926914.37.';
+        assert.equal(solver.validate(input), false);
+    });
+
+    // Logic handles a puzzle string that is not 81 characters in length
+    test('invalid puzzle string length', () => {
+        let input = '1.5..2.84..63.12.7.2..5.....9..1....8.2.367';
+        assert.equal(solver.validate(input), false);
+    });
 });
